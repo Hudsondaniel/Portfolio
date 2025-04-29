e4042ec58eceb3dae7af810e917f78f9f2e47df7
Hudson Daniel <60006022+Hudsondaniel@users.noreply.github.com>
2025-04-29 11:24:07 +0530

    Added a new card to feature the portfolio in itself

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -43,13 +43,15 @@ export default async function ProjectsPage() {
   const featured = allProjects.find((project) => project.slug === "sketchcode")!;
   const top2 = allProjects.find((project) => project.slug === "skynet")!;
   const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  const top4 = allProjects.find((project) => project.slug === "access")!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
       (project) =>
         project.slug !== featured.slug &&
         project.slug !== top2.slug &&
-        project.slug !== top3.slug,
+        project.slug !== top3.slug &&
+        project.slug !== top4.slug,
     )
     .sort(
       (a, b) =>
@@ -71,7 +73,7 @@ export default async function ProjectsPage() {
         </div>
         <div className="w-full h-px bg-zinc-800" />
 
-        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2">
           <Card>
             <Link href={`/projects/${featured.slug}`}>
               <article className="relative w-full h-full p-4 md:p-8">
@@ -119,7 +121,7 @@ export default async function ProjectsPage() {
             </Link>
           </Card>
 
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0">
             {[top2, top3].map((project) => (
               <Card key={project.slug}>
                 <Article project={project} views={views[project.slug] ?? 0} />
@@ -127,6 +129,13 @@ export default async function ProjectsPage() {
             ))}
           </div>
         </div>
+
+        <div className="mx-auto mt-8">
+          <Card>
+            <Article project={top4} views={views[top4.slug] ?? 0} />
+          </Card>
+        </div>
+
         <div className="hidden w-full h-px md:block bg-zinc-800" />
 
         <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
